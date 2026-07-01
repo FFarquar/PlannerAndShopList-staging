1b5f4e8af416e1e871e931648848dc49f1230610
FFarquar <FFarquar@users.noreply.github.com>
2026-07-01 01:51:23 +0000

    deploy: FFarquar/PlannerAndShopList@ead0d75e558321987b872c08712e7319031bd011

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bumped automatically by CI on every deploy (sed replaces this line)
-const CACHE_NAME = 'planner-v17';
+const CACHE_NAME = 'planner-v18';
 
 const STATIC_ASSETS = [
   './login.html',
